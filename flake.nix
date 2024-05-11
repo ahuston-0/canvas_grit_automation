0f3fddb19be2e1daf413ee538ab7d4d9be8011db
ahuston-0 <aliceghuston@gmail.com>
2024-05-11 13:54:41 -0400

    formatting and nix flake update
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- flake.nix
+++ flake.nix
@@ -13,109 +13,111 @@
     };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    utils,
-    ...
-  }:
-  # eachDefaultSystem is effectively a copy and paste of the same config multiple times,
-  # with the main difference being the system, which for our purposes includes x86 and arm64 linux.
-  # should allow for users of nix on macos to build there, but I'm not testing it.
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix {inherit pkgs;};
-      machine_shop = self.packages.${system}.machine_shop;
-      sync_cmd = "${machine_shop}/bin/sync";
-      overrides = poetry2nix.overrides.withDefaults (final: prev: {
-        # prefer binary wheels instead of source distributions for rust based dependencies
-        # avoids needing to build them from source. technically a security risk
-        polars = prev.polars.override {
-          preferWheel = true;
-        };
-        ruff = prev.ruff.override {
-          preferWheel = true;
-        };
-        # override to make sure setuptools is present for the canvasapi package
-        canvasapi = prev.canvasapi.overridePythonAttrs (old: {
-          buildInputs = (old.buildInputs or []) ++ [prev.setuptools];
-        });
-        # override to make sure the flit build system is present for itsdangerous
-        itsdangerous = prev.itsdangerous.overridePythonAttrs (old: {
-          buildInputs = (old.buildInputs or []) ++ [prev.flit];
-        });
-      });
-      # if you don't want the default that nixpkgs is supplying to you (as of april 2022, 3.11.8),
-      # you must manually specify it. the desired version will not be sourced from poetry.lock
-      python_version = pkgs.python312;
-    in {
-      packages = {
-        # defines how to build the docker image. by referencing sync_cmd, which references
-        # machine_shop (a package defined below), we guarantee that everthing needed is included
-        # in the docker image without needing to specify it.
-        dockerImage = pkgs.dockerTools.buildImage {
-          name = "sync";
-          tag = "latest";
-          config = {
-            Cmd = [sync_cmd];
-          };
-        };
-
-        # defines the poetry application that we want to build
-        # reads in pyproject.toml and poetry.lock to do this
-        machine_shop = poetry2nix.mkPoetryApplication {
-          projectDir = ./.;
-          overrides = overrides;
-          python = python_version;
-        };
-
-        # a nix idiom where default has a special meaning, so if you run
-        # nix build . then this is what will get built
-        default = machine_shop;
-      };
-
-      # defines the default development environment we want nix to make us
-      # it's essentially identical to the machine_shop package, except we specify
-      # the three modules we want to be editable in the development environment.
-      # if this was not done, you'd need to rebuild the shell every time you make a
-      # change and want to see it reflected, and debugging would be horrific (believe me I tried it)
-      # the editable packages correspond to what's in pyproject.toml
-      devShells.default = let
-        env = poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
-          overrides = overrides;
-          python = python_version;
-          editablePackageSources = {
-            sync = ./sync;
-            jsonl_formatter = ./jsonl_formatter;
-          };
-        };
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      utils,
+      ...
+    }:
+    # eachDefaultSystem is effectively a copy and paste of the same config multiple times,
+    # with the main difference being the system, which for our purposes includes x86 and arm64 linux.
+    # should allow for users of nix on macos to build there, but I'm not testing it.
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+        machine_shop = self.packages.${system}.machine_shop;
+        sync_cmd = "${machine_shop}/bin/sync";
+        overrides = poetry2nix.overrides.withDefaults (
+          final: prev: {
+            # prefer binary wheels instead of source distributions for rust based dependencies
+            # avoids needing to build them from source. technically a security risk
+            polars = prev.polars.override { preferWheel = true; };
+            ruff = prev.ruff.override { preferWheel = true; };
+            # override to make sure setuptools is present for the canvasapi package
+            canvasapi = prev.canvasapi.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ];
+            });
+            # override to make sure the flit build system is present for itsdangerous
+            itsdangerous = prev.itsdangerous.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ prev.flit ];
+            });
+          }
+        );
+        # if you don't want the default that nixpkgs is supplying to you (as of april 2022, 3.11.8),
+        # you must manually specify it. the desired version will not be sourced from poetry.lock
+        python_version = pkgs.python312;
       in
-        env.env.overrideAttrs (old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or [])
-            ++ [
+      {
+        packages = {
+          # defines how to build the docker image. by referencing sync_cmd, which references
+          # machine_shop (a package defined below), we guarantee that everthing needed is included
+          # in the docker image without needing to specify it.
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "sync";
+            tag = "latest";
+            config = {
+              Cmd = [ sync_cmd ];
+            };
+          };
+
+          # defines the poetry application that we want to build
+          # reads in pyproject.toml and poetry.lock to do this
+          machine_shop = poetry2nix.mkPoetryApplication {
+            projectDir = ./.;
+            overrides = overrides;
+            python = python_version;
+          };
+
+          # a nix idiom where default has a special meaning, so if you run
+          # nix build . then this is what will get built
+          default = machine_shop;
+        };
+
+        # defines the default development environment we want nix to make us
+        # it's essentially identical to the machine_shop package, except we specify
+        # the three modules we want to be editable in the development environment.
+        # if this was not done, you'd need to rebuild the shell every time you make a
+        # change and want to see it reflected, and debugging would be horrific (believe me I tried it)
+        # the editable packages correspond to what's in pyproject.toml
+        devShells.default =
+          let
+            env = poetry2nix.mkPoetryEnv {
+              projectDir = ./.;
+              overrides = overrides;
+              python = python_version;
+              editablePackageSources = {
+                sync = ./sync;
+                jsonl_formatter = ./jsonl_formatter;
+              };
+            };
+          in
+          env.env.overrideAttrs (old: {
+            nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
               # add extra packages you want available in the dev env
               pkgs.poetry
               pkgs.just
               pkgs.dive
               pkgs.nodePackages.prettier
             ];
-        });
+          });
 
-      formatter = pkgs.nixfmt-rfc-style;
+        formatter = pkgs.nixfmt-rfc-style;
 
-      # defines applications that could be ran with nix run
-      # currently goes unused, but is in interesting exercise in how you'd do it
-      apps = rec {
-        sync = {
-          type = "app";
-          program = sync_cmd;
+        # defines applications that could be ran with nix run
+        # currently goes unused, but is in interesting exercise in how you'd do it
+        apps = rec {
+          sync = {
+            type = "app";
+            program = sync_cmd;
+          };
+          default = sync;
         };
-        default = sync;
-      };
-    })
-      //
-      {hydraJobs = import ./hydra/jobs.nix {inherit (self) inputs outputs; };}
-  ;
+      }
+    )
+    // {
+      hydraJobs = import ./hydra/jobs.nix { inherit (self) inputs outputs; };
+    };
 }
--- hydra/jobs.nix
+++ hydra/jobs.nix
@@ -1,22 +1,19 @@
-{
-  inputs,
-  outputs,
-}: let
+{ inputs, outputs }:
+let
   inherit (inputs.nixpkgs) lib;
 
   notBroken = pkg: !(pkg.meta.broken or false);
-  isDistributable = pkg: (pkg.meta.license or {redistributable = true;}).redistributable;
-  hasPlatform = sys: pkg: lib.elem sys (pkg.meta.platforms or [sys]);
-  filterValidPkgs = sys: pkgs:
+  isDistributable = pkg: (pkg.meta.license or { redistributable = true; }).redistributable;
+  hasPlatform = sys: pkg: lib.elem sys (pkg.meta.platforms or [ sys ]);
+  filterValidPkgs =
+    sys: pkgs:
     lib.filterAttrs (
       _: pkg: lib.isDerivation pkg && hasPlatform sys pkg && notBroken pkg && isDistributable pkg
-    )
-    pkgs;
-
-  # getCfg = _: cfg: cfg.config.system.build.toplevel;
-in {
+    ) pkgs;
+in
+# getCfg = _: cfg: cfg.config.system.build.toplevel;
+{
   # hosts = lib.mapAttrs getCfg outputs.nixosConfigurations;
   formatter = outputs.formatter;
   packages = lib.mapAttrs filterValidPkgs outputs.packages;
-
 }
